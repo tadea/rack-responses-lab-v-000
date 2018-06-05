009df790345c4ed566eb701831dffe8621e61445
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-06-05 20:54:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
     resp = Rack::Response.new
 
     if Time.now.hour < 12
-      resp.write "Good morning!"
+      resp.write "Good Morning!"
     else
-      resp.write "Good afternoon!"
+      resp.write "Good Afternoon!"
     end
 
     resp.finish
