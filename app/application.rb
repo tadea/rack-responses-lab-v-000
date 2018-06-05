014db76bf36af8a0d9eccffaaa093d008a8aae85
Tadea Simunovic <tsimunoviczg@gmail.com>
2018-06-05 20:51:54 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,16 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+
+    if Time.now < 12
+      resp.write "Good morning!"
+    else
+      resp.write "Good afternoon!"
+    end
+
+    resp.finish
+  end
+
+  end
+
+end
